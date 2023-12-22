0e0af4bf34e40c6974fc03c1f85b2543fc2d8d3c
Ahmad Muhammad <theahmadm3@gmail.com>
2023-12-22 09:42:32 +0100

    Update password generator UI.

--- src/App.jsx
+++ src/App.jsx
@@ -42,57 +42,57 @@ const App = () => {
   };
 
   return (
-      <div className="mb5">
-        <header className="mw7 center ph5-l">
-          <h1>Random Password generator</h1>
-        </header>
-        <main className="mw7 center ph5-l">
-          <div>
-            <h1>
-              Generate a <br />
-              Random Password
-            </h1>
-          </div>
-          <section className="center pa2 mb4 f4">
-            <label htmlFor="length">
-              Length of the password:
-              <input
-                type="text"
-                id="length"
-                className="bn shade br1 pa2 bg-light-gray ml2-ns black outline-0 mt2 mb3"
-                placeholder="within 8 or 16"
-                value={passwordLength}
-                onChange={(e) => setPasswordLength(e.target.value)}
-              />
-            </label>
-            <p id="error" className="red fw-bolder shade mt2">
-              {errorMessage}
-            </p>
-          </section>
-          <div className="shadow-1 pa2 inline-flex justify-between w-100 br4 bg-white w-70-ns mb4">
+    <div className="mb5 pa2 w-100">
+      <header className="mw7 center ph5-l">
+        <h1>Random Password generator</h1>
+      </header>
+      <main className="mw7 center ph5-l">
+        <div>
+          <h1>
+            Generate a <br />
+            Random Password
+          </h1>
+        </div>
+        <section className="center pa2 mb4 f4">
+          <label htmlFor="length">
+            Length of the password:
             <input
-              className="bn w-90 bg-transparent f4 pa1 outline-0"
               type="text"
-              id="password"
-              placeholder="Password"
-              value={password}
-              readOnly
+              id="length"
+              className="bn shade br1 pa2 bg-light-gray ml2-ns black outline-0 mt2 mb3"
+              placeholder="within 8 or 16"
+              value={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
             />
-            <img
-              src={copyImg}
-              className="w-10 pointer"
-              alt="copy"
-              onClick={copyPassword}
-            />
-          </div>
-          <button
-            className="shadow-1 f4 pointer pa3 inline-flex justify-between w-100 br4 white bg-dark-blue w-70-ns bn"
-            onClick={generatePassword}
-          >
-            <span>Generate Password</span> <img src={generateImg} alt="generate" />
-          </button>
-        </main>
-      </div>
+          </label>
+          <p id="error" className="red fw-bolder shade mt2">
+            {errorMessage}
+          </p>
+        </section>
+        <div className="shadow-1 pa2 inline-flex justify-between w-100 br4 bg-white w-70-ns mb4">
+          <input
+            className="bn w-90 bg-transparent black f4 pa1 outline-0"
+            type="text"
+            id="password"
+            placeholder="Password"
+            value={password}
+            readOnly
+          />
+          <img
+            src={copyImg}
+            className="w-10 pointer"
+            alt="copy"
+            onClick={copyPassword}
+          />
+        </div>
+        <button
+          className="shadow-1 f4 pointer pa3 inline-flex justify-between w-100 br4 white bg-dark-blue w-70-ns bn"
+          onClick={generatePassword}
+        >
+          <span>Generate Password</span> <img src={generateImg} alt="generate" />
+        </button>
+      </main>
+    </div>
   );
 };
 
