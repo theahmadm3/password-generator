bce718df9adb8cc561c8ad8d8a09822531587c05
Ahmad Muhammad <theahmadm3@gmail.com>
2023-12-22 09:47:44 +0100

    ughh

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return ( 
         <footer className="bg-dark-gray pa2 inline-flex justify-between w-100">
             <p className="w-50 inline-flex white justify-between">&copy; <a href="github/theahmadm3">theahmadm3</a> </p>
-            <p className="w-50 inline-flex justify-end">2023</p>
+            <p className="w-50 white inline-flex justify-end">2023</p>
         </footer>
      );
 }
